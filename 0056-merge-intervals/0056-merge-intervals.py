a957a2d7e15340e4fa751ad086637f1d4168f504
Aadil Saiyed <aadil.saiyed0327@gmail.com>
2026-07-08 20:26:24 +0530

    Time: 11 ms (25.04%), Space: 23.3 MB (7.19%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,13 +1,17 @@
 class Solution:
     def merge(self, nums: List[List[int]]) -> List[List[int]]:
+        #1 Sort the array
         nums.sort()
         ans=[]
         n = len(nums)
+
+        #2 iterate over for matching adjacent intervals
         for i in range(n):
             start=nums[i][0]
             end = nums[i][1]
-            if ans and end <= ans[-1][1]:
+            if ans and end <= ans[-1][1]: # checking adjacent intervals
                 continue
+            #3 handling interval chaining (multiple overlapping)
             for j in range(i+1,n):
                 if end >= nums[j][0]:
                     end = max(end,nums[j][1])
