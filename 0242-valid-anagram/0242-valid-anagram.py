3924f372b9298ce92333c66e8e045aa5725ff3df
Aadil Saiyed <aadil.saiyed0327@gmail.com>
2026-07-26 23:07:00 +0530

    Time: 14 ms (48.13%), Space: 19.2 MB (99.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return "".join(sorted(s)) == "".join(sorted(t))
+        if len(s)!=len(t):
+            return False
+        n=len(s)
+        mp={}
+        for i in s:
+            if i in mp:
+                mp[i]+=1
+            else:
+                mp[i]=1
+        for i in t:
+            if i in mp:
+                mp[i]-=1
+            else:
+                mp[i]=-1
+            if mp[i]==0:
+                mp.pop(i)
+        if mp == {}:
+            return True
+        return False
+        
+        
