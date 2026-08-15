9d2767e276622ec804ed7ea76ace464efd4eab8f
Aadil Saiyed <aadil.saiyed0327@gmail.com>
2026-08-15 13:57:14 +0530

    Time: 181 ms (39.34%), Space: 40.6 MB (57.85%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,15 +5,39 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        arr = []
-        temp = head
-        while temp:
-            arr.append(temp.val)
-            temp = temp.next
-        arr.sort()
-        temp = head
-        for i in range(len(arr)):
-            temp.val = arr[i]
-            temp = temp.next
-        return head
+        def middle(head):
+            slow,fast=head,head.next
+            while fast and fast.next:
+                slow=slow.next
+                fast = fast.next.next
+            return slow
+        def merge(L1,L2):
+            dummy = ListNode()
+            tail = dummy
             
+            while L1 and L2:
+                if L1.val < L2.val:
+                    tail.next=L1
+                    L1=L1.next
+                else:
+                    tail.next = L2
+                    L2=L2.next
+                tail = tail.next
+            if L1:
+                tail.next = L1
+            else:
+                tail.next = L2
+            return dummy.next     
+
+        def mergeSort(head):
+            if head is None or head.next is None:
+                return head
+            mid = middle(head)
+            LeftHead = head
+            RightHead = mid.next
+            mid.next=None
+            SortedL=mergeSort(LeftHead)
+            SortedR=mergeSort(RightHead)
+            return merge(SortedL,SortedR)
+        return mergeSort(head)
+        
