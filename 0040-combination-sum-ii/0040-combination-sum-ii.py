5d1c3c1253f9bc039e5e7becec2d860afedf42d2
Aadil Saiyed <aadil.saiyed0327@gmail.com>
2026-09-05 11:05:47 +0530

    Time: 30 ms (20.91%), Space: 19.5 MB (43.21%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -4,14 +4,16 @@ class Solution:
         candidates.sort()
         def backtrack(idx,target,subarr):
             if target==0:
-                res.append(subarr)
+                res.append(subarr[:])
                 return
             if target<0 or idx == len(candidates):
                 return
-            backtrack(idx+1,target-candidates[idx],subarr+[candidates[idx]])
+            subarr.append(candidates[idx]) # PICK
+            backtrack(idx+1,target-candidates[idx],subarr) #Explore PICK
+            subarr.pop() # UNDO PICK
             nextidx=idx+1
             while nextidx<len(candidates) and candidates[idx]==candidates[nextidx]:
                 nextidx+=1
-            backtrack(nextidx,target,subarr)
+            backtrack(nextidx,target,subarr) # Explore NOT PICK
         backtrack(0,target,[])
         return res
